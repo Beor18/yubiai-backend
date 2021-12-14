693d7ab1d203d30325ea5c4c05f0b2414b395d62
Fernando Lopez <lopezfer@globalhitss.com>
2021-12-14 20:32:56 -0300

    gitignore

--- src/server.js
+++ src/server.js
@@ -44,32 +44,32 @@ app.listen(4000, () => {
   console.log("Server running on port 4000");
 });
 
-const listenToEvents = () => {
-  const provider = new ethers.providers.JsonRpcProvider(
-    "http://localhost:8545"
-  );
+// const listenToEvents = () => {
+//   const provider = new ethers.providers.JsonRpcProvider(
+//     "http://localhost:8545"
+//   );
   
-  const ppAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+//   const ppAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
-  const paymentProcessor = new ethers.Contract(
-    ppAddress,
-    PaymentProcessor.abi,
-    provider
-  );
+//   const paymentProcessor = new ethers.Contract(
+//     ppAddress,
+//     PaymentProcessor.abi,
+//     provider
+//   );
 
-  paymentProcessor.on("PaymentDone", async (payer, amount, paymentId, date) => {
-    console.log(`
-          from ${payer}
-          amount ${amount}
-          paymentId ${paymentId}
-          date ${new Date(date.toNumber() * 1000).toLocaleString()}
-        `);
-    const payment = await Payment.findOne({ id: paymentId });
-    if (payment) {
-      payment.paid = true;
-      await payment.save();
-    }
-  });
-};
+//   paymentProcessor.on("PaymentDone", async (payer, amount, paymentId, date) => {
+//     console.log(`
+//           from ${payer}
+//           amount ${amount}
+//           paymentId ${paymentId}
+//           date ${new Date(date.toNumber() * 1000).toLocaleString()}
+//         `);
+//     const payment = await Payment.findOne({ id: paymentId });
+//     if (payment) {
+//       payment.paid = true;
+//       await payment.save();
+//     }
+//   });
+// };
 
-listenToEvents();
+// listenToEvents();
